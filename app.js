f6c35f958dbaf6fa711fff45aadc5b17c71bdbfb
McMartinezc <psyence_mc@hotmail.com>
2023-10-24 11:55:58 +0200

    ok

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // Pika-pika suerte!
+const colors = document.getElementById("colors");
+const pikachu = document.querySelector("img");
+
+colors.addEventListener("click", (event)=>{
+    const selectColor= event.target.id;
+    pikachu.style.background = selectColor;
+})
